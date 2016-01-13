daf9190e0de38d9711d613c4ef053fbb6ec2f076
Yew Hong <yewhong_ng91@hotmail.com>
2016-01-13 14:18:43 +0800

    Design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
   before_action :require_login
 
   def index
-  	if current_user
-  		redirect_to users_path
-  	end
+  	@users = User.all
   end
 
   def edit
