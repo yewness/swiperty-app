95006946f37bd5032c29219f39831ce857b6053a
Yew Hong <yewhong_ng91@hotmail.com>
2016-01-14 01:27:49 +0800

    Methods Friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,23 @@ class User < ActiveRecord::Base
 		)
 	end
 
+	def request_match(user_2)
+		self.friendships.create(friend: user_2)
+	end
+
+	def accept_friendship
+		self.update_attributes(state: "active", friended_at: Time.now)
+	end
+
+	def remove_match(user2)
+		inverse_friendship = inverse_friendships.where(user_id: user2).first
+		if inverse_friendship
+			self.inverse_friendships.where(user_id: user2).first.destroy
+		else
+			self.friendships.where(friend_id: user2).first.destroy
+		end
+	end
+	
 	private
 	def self.process_uri(uri)
 		image_url = URI.parse(uri)
