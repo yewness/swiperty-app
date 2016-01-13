1f38719a186cc4a9c496597bfb69365798c76b5a
Yew Hong <yewhong_ng91@hotmail.com>
2016-01-14 01:12:07 +0800

    Friendship Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
 	default_scope { order('id DESC')}
+	has_many :friendships, dependent: :destroy
+	has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
 
 	has_attached_file :avatar,
 					storage: :s3,
